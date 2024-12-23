5723a0664d6bf997bef6033a134c7a45d0ff170f
Congphu9824 <phamphu872004@gmail.com>
2024-12-23 17:59:32 +0700

    Add project files.

---
+++ Data/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Image { get; set; }
+        public string Status { get; set; }
+        public decimal Salary { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; } // Có thể định nghĩa là enum nếu cần
+        public string? Class { get; set; }
+        public string? Position { get; set; }
+        public string? Designation { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
